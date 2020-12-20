f26eae0320e3facb9451301f2fc695ea8542602e
Ian <45278651+iankhou@users.noreply.github.com>
2020-12-20 00:39:57 -0700

    rudimentary socket connection to frontend

--- dist-server/controllers/sampleBoard.js
+++ dist-server/controllers/sampleBoard.js
@@ -4,6 +4,11 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var sampleBoard = [];
+var sampleBoard = {
+  name: 'sample',
+  positions: [],
+  playerOnePositions: [],
+  playerTwoPositions: []
+};
 var _default = sampleBoard;
 exports["default"] = _default;
