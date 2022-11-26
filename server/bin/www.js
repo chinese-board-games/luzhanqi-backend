21a94db1f057f90ba09b7ca35126991ea5131ee7
Ian <45278651+iankhou@users.noreply.github.com>
2022-11-26 02:14:04 -0500

    add frontend to cors allow

--- server/bin/www.js
+++ server/bin/www.js
@@ -8,12 +8,12 @@
  * Module dependencies.
  */
 
-const debugLib = require('debug');
-const http = require('http');
-const app = require('../app');
-const lqz = require('../lzqgame');
+const debugLib = require("debug");
+const http = require("http");
+const app = require("../app");
+const lqz = require("../lzqgame");
 
-const debug = debugLib('your-project-name:server');
+const debug = debugLib("your-project-name:server");
 
 /**
  * Create HTTP server.
@@ -23,17 +23,21 @@ const server = http.createServer(app);
 const options = {
   maxHttpBufferSize: 1e8,
   cors: {
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://lzq.surge.sh'],
-    methods: ['GET', 'POST'],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://lzq.surge.sh",
+    ],
+    methods: ["GET", "POST"],
   },
 };
 
 // io.adapter(redisAdapter({ host: 'localhost', port: 6379 }));
-const io = require('socket.io')(server, options);
+const io = require("socket.io")(server, options);
 
 // eslint-disable-next-line prefer-arrow-callback
-io.sockets.on('connection', function (socket) {
-  console.log('client connected');
+io.sockets.on("connection", function (socket) {
+  console.log("client connected");
   lqz.initGame(io, socket);
 });
 
@@ -41,30 +45,31 @@ io.sockets.on('connection', function (socket) {
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(process.env.PORT || 3000, '0.0.0.0');
-server.on('error', onError);
-server.on('listening', onListening);
+server.listen(process.env.PORT || 3000, "0.0.0.0");
+server.on("error", onError);
+server.on("listening", onListening);
 
 /**
  * Event listener for HTTP server "error" event.
  */
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  const bind = typeof port === 'string'
-    ? `Pipe ${process.env.PORT || 3000}`
-    : `Port ${process.env.PORT || 3000}`;
+  const bind =
+    typeof port === "string"
+      ? `Pipe ${process.env.PORT || 3000}`
+      : `Port ${process.env.PORT || 3000}`;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
+    case "EACCES":
       console.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
-    case 'EADDRINUSE':
+    case "EADDRINUSE":
       console.error(`${bind} is already in use`);
       process.exit(1);
       break;
@@ -79,9 +84,7 @@ function onError(error) {
 
 function onListening() {
   const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? `pipe ${addr}`
-    : `port ${addr.port}`;
+  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
   console.log(`Listening on ${bind}`);
   debug(`Listening on ${bind}`);
 }
