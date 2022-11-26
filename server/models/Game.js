c7bce66275370237e6d7d43ac1bd3863d0fcea66
fireteam99 <raymancheungsy@gmail.com>
2022-11-25 23:06:57 -0800

    moved utils over and updated some stuff

--- server/models/Game.js
+++ server/models/Game.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const GameSchema = new Schema({
-  room: String,
-  host: String,
-  players: [],
-  moves: [],
-  turn: Number,
-  board: [],
-}, { timestamps: true });
+const GameSchema = new Schema(
+  {
+    room: String,
+    host: String,
+    players: [],
+    moves: [],
+    turn: Number,
+    board: [],
+  },
+  { timestamps: true }
+);
 
 const GameModel = mongoose.model('Game', GameSchema);
 
